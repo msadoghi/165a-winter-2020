7c9f6cd3603dcf0a32011a5c187f2b48eb453c51
Parsoa Khorsand Rahim Zadeh <pkhorsand@rum.hpc.genomecenter.ucdavis.edu>
2020-03-04 12:09:37 -0800

    Add single-stage M3 tester, restore testers for previous milestones.

--- template/m3_tester.py
+++ template/m3_tester.py
@@ -7,48 +7,59 @@ from random import choice, randint, sample, seed
 
 db = Database()
 db.open('/home/pkhorsand/165a-winter-2020-private/db')
-grades_table = db.get_table('Grades')
+grades_table = db.create_table('Grades', 5, 0)
 
 keys = []
 records = {}
 seed(3562901)
 num_threads = 8
 
-# generate records already in database
-for i in range(0, 1000):
-    key = 92106429 + i
-    keys.append(key)
-    records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
+try:
+    grades_table.index.create_index(1)
+    grades_table.index.create_index(2)
+    grades_table.index.create_index(3)
+    grades_table.index.create_index(4)
+except Exception as e:
+    print('Index API not imlemented properly, tests may fail.')
 
 transaction_workers = []
+insert_transactions = []
 select_transactions = []
 update_transactions = []
 for i in range(num_threads):
+    insert_transactions.append(Transaction())
     select_transactions.append(Transaction())
     update_transactions.append(Transaction())
     transaction_workers.append(TransactionWorker())
+    transaction_workers[i].add_transaction(insert_transactions[i])
     transaction_workers[i].add_transaction(select_transactions[i])
     transaction_workers[i].add_transaction(update_transactions[i])
-worker_keys = [ {} for worker in transaction_workers ]
+worker_keys = [ {} for t in transaction_workers ]
+
+for i in range(0, 1000):
+    key = 92106429 + i
+    keys.append(key)
+    i = i % num_threads
+    records[key] = [key, randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20), randint(i * 20, (i + 1) * 20)]
+    q = Query(grades_table)
+    insert_transactions[i].add_query(q.insert, *records[key])
+    worker_keys[i][key] = True
 
 t = 0
 _records = [records[key] for key in keys]
 for c in range(grades_table.num_columns):
-    _keys = list(set([record[c] for record in _records]))
+    _keys = sorted(list(set([record[c] for record in _records])))
     index = {v: [record for record in _records if record[c] == v] for v in _keys}
     for key in _keys:
-        found = False
-        for j, key_set in enumerate(worker_keys):
-            if j != t % num_threads and any(map(lambda r: r[0] in key_set, index[key])):
-                found = True
-                break
-        if not found:
+        found = True
+        for record in index[key]:
+            if record[0] not in worker_keys[t % num_threads]:
+                found = False
+        if found:
             query = Query(grades_table)
             select_transactions[t % num_threads].add_query(query.select, key, c, [1, 1, 1, 1, 1])
-            worker_keys[t % num_threads].update({record[0]: True for record in index[key]})
         t += 1
 
-t = 0
 for j in range(0, num_threads):
     for key in worker_keys[j]: 
         updated_columns = [None, None, None, None, None]
@@ -57,8 +68,7 @@ for j in range(0, num_threads):
             updated_columns[i] = value
             records[key][i] = value
             query = Query(grades_table)
-            update_transactions[t % num_threads].add_query(query.update, key, *updated_columns)
-            t += 1
+            update_transactions[j].add_query(query.update, key, *updated_columns)
             updated_columns = [None, None, None, None, None]
 
 for transaction_worker in transaction_workers:
